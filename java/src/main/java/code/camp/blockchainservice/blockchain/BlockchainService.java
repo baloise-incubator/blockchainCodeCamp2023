71bda24813b62e3fe92c5f2f6e59d80ec4d2bf78
Kolja Junginger <kolja.junginger@baloise.ch>
2023-04-25 16:40:20 +0200

    create java class from solidity contract file

--- java/src/main/java/code/camp/blockchainservice/blockchain/BlockchainService.java
+++ java/src/main/java/code/camp/blockchainservice/blockchain/BlockchainService.java
@@ -4,17 +4,25 @@ import code.camp.blockchainservice.controller.SmartContract;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
 
 @Service
 @Slf4j
 public class BlockchainService {
 
-    private Logger _logger = LoggerFactory.getLogger(BlockchainService.class);
+    private static final String TOKEN = "INFURA_TOKEN";
+    private final Logger _logger = LoggerFactory.getLogger(BlockchainService.class);
 
     public SmartContract getSmartContractByHash(Long smartContractHash) {
+
+        var accessToken = System.getenv(TOKEN);
+        var web3j = Web3j.build(new HttpService("https://rinkeby.infura.io/" + accessToken));
+
+        Credentials credentials;
+//        BaloiseLifeInsurance.deploy(web3j, credentials);
 
         _logger.info("calling Etherium TEST blockchain for hash : " + smartContractHash);
         // implement API call to Etherium TEST blockchain
